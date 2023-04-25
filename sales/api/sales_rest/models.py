bc2ac0728b21fb526ce1c66fe3b0991970607405
jonathan pinon <jonathanjosephpinon@gmail.com>
2023-04-25 00:18:42 -0700

    sales models done

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Salesperson(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    employee_id = models.CharField(max_length=50)
+
+    def get_api_url(self):
+        return reverse("salesperson", kwargs={"pk": self.id})
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=12)
+
+    def get_api_url(self):
+        return reverse("customer", kwargs={"pk": self.id})
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+
+    def get_api_url(self):
+        return reverse("automobilevo", kwargs={"pk": self.id})
+
+class Sale(models.Model):
+    price = models.IntegerField(null=True)
+    customer = models.ForeignKey(
+        Customer,
+        related_name="customer",
+        on_delete=models.CASCADE
+    )
+    salesperson = models.ForeignKey(
+        Salesperson,
+        related_name="salesperson",
+        on_delete=models.CASCADE
+    )
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        related_name="automobile",
+        on_delete=models.CASCADE
+    )
+    def get_api_url(self):
+        return reverse("sale", kwargs={"pk": self.id})
